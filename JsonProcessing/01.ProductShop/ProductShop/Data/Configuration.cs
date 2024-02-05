a924e5c99a9b3921c5285b73987cb138cb772c66
Antoan Furlanski <antoan.furlanski@gmail.com>
2024-02-05 18:36:48 +0200

    Update Configuration.cs

--- JsonProcessing/01.ProductShop/ProductShop/Data/Configuration.cs
+++ JsonProcessing/01.ProductShop/ProductShop/Data/Configuration.cs
@@ -3,6 +3,6 @@
     public static class Configuration
     {
         public const string ConnectionString =
-			@"Server=DESKTOP-Q7KF8R7\SQLEXPRESS;Database=ProductShop;Integrated Security=True";
+			@"Server=.;Database=ProductShop;Integrated Security=True";
     }
 }
