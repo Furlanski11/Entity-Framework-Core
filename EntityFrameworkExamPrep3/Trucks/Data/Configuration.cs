abd814ae1f1c15caf02e7331033a66b86bd3ee2d
Antoan Furlanski <antoan.furlanski@gmail.com>
2024-02-05 18:35:06 +0200

    Update Configuration.cs

--- EntityFrameworkExamPrep3/Trucks/Data/Configuration.cs
+++ EntityFrameworkExamPrep3/Trucks/Data/Configuration.cs
@@ -2,6 +2,6 @@
 {
     public static class Configuration
     {
-        public static string ConnectionString = @"Server=DESKTOP-Q7KF8R7\SQLEXPRESS;Database=Trucks;Trusted_Connection=True";
+        public static string ConnectionString = @"Server=.;Database=Trucks;Trusted_Connection=True";
     }
 }
